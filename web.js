89b8101dd1a6ebc7d1932ea405d3f842f1741ee3
Aman Kapoor <99702502+MrAmanKapoor@users.noreply.github.com>
2022-05-05 11:36:38 +0530

    Create web.js

---
+++ web.js
@@ -0,0 +1,79 @@
+function Samsung(){
+    document.getElementById("Samsung").style.display="block";
+    document.getElementById("Redmi").style.display="none";
+    document.getElementById("Sonata").style.display="none";
+    document.getElementById("Boat").style.display="none";
+}
+
+function Redmi(){
+    document.getElementById("Samsung").style.display="none";
+    document.getElementById("Redmi").style.display="block";
+    document.getElementById("Sonata").style.display="none";
+    document.getElementById("Boat").style.display="none";
+}
+
+function Sonata(){
+    document.getElementById("Samsung").style.display="none";
+    document.getElementById("Redmi").style.display="none";
+    document.getElementById("Sonata").style.display="block";
+    document.getElementById("Boat").style.display="none";
+}
+
+function Boat(){
+    document.getElementById("Samsung").style.display="none";
+    document.getElementById("Redmi").style.display="none";
+    document.getElementById("Sonata").style.display="none";
+    document.getElementById("Boat").style.display="block";
+}
+
+function Samsung1(){
+    document.getElementById("Samsung1").style.display="block";
+}
+function Samsung2(){
+    document.getElementById("Samsung2").style.display="block";
+}
+function Samsung3(){
+    document.getElementById("Samsung3").style.display="block";
+}
+function Samsung4(){
+    document.getElementById("Samsung4").style.display="block";
+}
+function Samsung5(){
+    document.getElementById("Samsung5").style.display="block";
+}
+function Redmi1(){
+    document.getElementById("Redmi1").style.display="block";
+}
+function Redmi2(){
+    document.getElementById("Redmi2").style.display="block";
+}
+function Redmi3(){
+    document.getElementById("Redmi3").style.display="block";
+}
+function Redmi4(){
+    document.getElementById("Redmi4").style.display="block";
+}
+function Sonata1(){
+    document.getElementById("Sonata1").style.display="block";
+}
+function Sonata2(){
+    document.getElementById("Sonata2").style.display="block";
+}
+function Sonata3(){
+    document.getElementById("Sonata3").style.display="block";
+}
+function Sonata4(){
+    document.getElementById("Sonata4").style.display="block";
+}
+function Boat1(){
+    document.getElementById("Boat1").style.display="block";
+}
+function Boat2(){
+    document.getElementById("Boat2").style.display="block";
+}
+function Boat3(){
+    document.getElementById("Boat3").style.display="block";
+}
+function Boat4(){
+    document.getElementById("Boat4").style.display="block";
+}
